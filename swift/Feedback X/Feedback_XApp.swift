5736c50ad94770bf8529a363809bdac7914f8ac5
LeonHTM <leon.gabriel.strauss@icloud.com>
2024-12-11 10:43:35 +0100

    added swift

---
+++ swift/Feedback X/Feedback_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feedback_XApp.swift
+//  Feedback X
+//
+//  Created by Leon  on 10.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Feedback_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
