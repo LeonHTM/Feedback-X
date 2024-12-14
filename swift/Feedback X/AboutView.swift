502532c2e112350949f6e9bbaec1b78cb8cbc989
LeonHTM <leon.gabriel.strauss@icloud.com>
2024-12-14 21:41:25 +0100

    introduce SideBar in Swift WIndow

---
+++ swift/Feedback X/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Feedback X
+//
+//  Created by Leon  on 14.12.2024.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
---
+++ swift/Feedback X/SidebarView.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarView.swift
+//  Feedback X
+//
+//  Created by Leon  on 14.12.2024.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SidebarView()
+}
