72f0c6128f66237c557f47ea0e30e44fe0cbd266
LeonHTM <leon.gabriel.strauss@icloud.com>
2024-12-18 11:55:05 +0100

    added testing pages

---
+++ swift/Feedback X/TestCycleView.swift
@@ -0,0 +1,19 @@
+//
+//  TestCycleView.swift
+//  Feedback X
+//
+//  Created by Leon  on 18.12.2024.
+//  Copyright © 2024 LeonHTM. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestCycleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestCycleView()
+}
---
+++ swift/Feedback X/TestRewriteView.swift
@@ -0,0 +1,19 @@
+//
+//  TestRewriteView.swift
+//  Feedback X
+//
+//  Created by Leon  on 18.12.2024.
+//  Copyright © 2024 LeonHTM. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestRewriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestRewriteView()
+}
