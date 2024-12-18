51f1ce37460f032c47ab0e6c2211289a2bd19499
LeonHTM <leon.gabriel.strauss@icloud.com>
2024-12-18 18:55:58 +0100

    added sheet to enter details for cycle

--- swift/Feedback X/CreateFeedbackView.swift
+++ swift/Feedback X/CreateFeedbackView.swift
@@ -3,8 +3,12 @@ import SwiftUI
 struct CreateFeedbackView: View {
     @State private var isRunning = false
     @State private var showAlert = false
-    @AppStorage("HasShownAlert") var hasShownAlert: Bool = false 
+    @State private var showSheet = false
+    @State private var feedbackTitle: String = ""
+    @State private var selectedOption = "Option 1"
+    @AppStorage("HasShownAlert") var hasShownAlert: Bool = false
     @AppStorage("AppLaunchCounter") var appLaunchCounter: Int = 1
+    
 
     var body: some View {
         VStack {
@@ -21,18 +25,57 @@ struct CreateFeedbackView: View {
 
             Button(action: {
                 ///runPythonScript(scriptPath: "/Users/leon/Desktop/Feedback-X/python/main.py",completion:True)
+                showSheet = true
+                
             }) {
                 Text(isRunning ? "Running..." : "Run Python Script")
-                    .padding()
+                    .padding(1)
+
             }
-            .background(isRunning ? Color.gray : Color.accentColor)
-            .buttonStyle(PlainButtonStyle())
-            .cornerRadius(10)
+            
+            //.background(isRunning ? Color.gray : Color.accentColor)
+            //.buttonStyle(PlainButtonStyle())
+            //.cornerRadius(10)
             .disabled(isRunning)
             .padding()
+            .sheet(isPresented: $showSheet) {
+                ScrollView{
+                    VStack (alignment: .leading,spacing:10){
+                        Text("Basic Information")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("Please provide a descriptive title for your feedback")
+                        TextField("", text: $feedbackTitle)
+                        Text("Example: Unable to make phone calls from lock screen in iOS 18.2 (22C152)")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12))
+                        Text("Which Area are you seings an Issue with?")
+                        List {
+                            Picker(selection: $selectedOption) {
+                                Text("Chocolate")
+                                Text("Vanilla")
+                                Text("Strawberry")
+                            } label: {
+                                Text("Flavor")
+                                Text("Choose your favorite flavor")
+                            }
+                        }
+                        
+                        
+                        
+                        Button("OK") {
+                            showSheet = false
+                        }
+                        .padding()
+                    }
+                    .padding(.horizontal,60)
+                    .padding(.vertical,20)
+                }
+            }
 
-            Image(systemName: "exclamationmark.bubble")
-                .imageScale(.large)
+            
+            
+
         }
         .alert("Legal Notice", isPresented: $showAlert) {
             Button("Quit Feedback X", role: .cancel) {
