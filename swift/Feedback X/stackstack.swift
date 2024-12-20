5f08a8f4df0d97d62d582a6267a84f317ab68807
LeonHTM <leon.gabriel.strauss@icloud.com>
2024-12-20 22:19:21 +0100

    Swift: Added loading of Saves/; Python: Changed how files are saved as txt. (Title now comes first and date and time are on seperate lines).

---
+++ swift/Feedback X/stackstack.swift
@@ -0,0 +1,19 @@
+//
+//  stackstack.swift
+//  Feedback X
+//
+//  Created by Leon  on 20.12.2024.
+//  Copyright © 2024 LeonHTM. All rights reserved.
+//
+
+import SwiftUI
+
+struct stackstack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    stackstack()
+}
