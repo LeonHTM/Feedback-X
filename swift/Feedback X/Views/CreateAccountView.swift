0f6add6511fe68d6213303f49e98af744040e63d
LeonHTM <leon.gabriel.strauss@icloud.com>
2024-12-25 22:59:55 +0100

    Merry Christmas 3.0: Added Edit and Delete Button to AccountsView

---
+++ swift/Feedback X/Views/CreateAccountView.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountView.swift
+//  Feedback X
+//
+//  Created by Leon  on 25.12.2024.
+//  Copyright © 2024 LeonHTM. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAccountView()
+}
