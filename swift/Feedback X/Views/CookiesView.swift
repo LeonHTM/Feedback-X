6f04e364b2b93b9fdd2937a55bb177d2257355da
LeonHTM <leon.gabriel.strauss@icloud.com>
2025-01-04 21:42:34 +0100

    Added CookiesView

---
+++ swift/Feedback X/Views/CookiesView.swift
@@ -0,0 +1,19 @@
+//
+//  CookiesView.swift
+//  Feedback X
+//
+//  Created by Leon  on 04.01.2025.
+//  Copyright © 2025 LeonHTM. All rights reserved.
+//
+
+import SwiftUI
+
+struct CookiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CookiesView()
+}
