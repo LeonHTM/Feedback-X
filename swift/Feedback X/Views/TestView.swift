2e641d9fa6332ef657486f8c433f1f4d9ce54a0e
LeonHTM <leon.gabriel.strauss@icloud.com>
2025-02-08 18:24:24 +0100

    succa blyad

--- swift/Feedback X/Views/TestView.swift
+++ swift/Feedback X/Views/TestView.swift
@@ -11,28 +11,9 @@ import SwiftUI
 
 struct TestView: View {
     
-    @State var text: String = "MacBookAir10,1"
-    @State var text2: String = "MacBookAir10,1"
-    
-    @State private var sliderSave: Double = 4
-    @State private var accountNumber: Double = 100
     
     var body: some View {
-        Menu {
-            Button(action: {
-                // Action for the first menu item
-                sliderSave = 200
-            }) {
-                Label("Option 1", systemImage: "star")
-            }
       
-        } label: {
-            Text("Tap me")
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
-        }
         Text(VersionBuild.getAppVersion())
         Text(String(VersionBuild.getBuildNumber()))
         
