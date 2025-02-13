cd0a8811180f8bedf9fbad2307970ae1ea859643
LeonHTM <leon.gabriel.strauss@icloud.com>
2025-02-13 08:49:22 +0100

    fucked my life

---
+++ Feedback X/Feedback X/Logic/UpdateHandler.swift
@@ -0,0 +1,19 @@
+//
+//  UpdateHandler.swift
+//  Feedback X
+//
+//  Created by Leon  on 12.02.2025.
+//  Copyright © 2025 LeonHTM. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateHandler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateHandler()
+}
